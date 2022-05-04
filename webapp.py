1e3a70846e212ad91ffef6df4504aa35750eaf15
christopher graner <chrisgraner1@gmail.com>
2022-05-04 09:18:44 -0700

    Changes variables and added a question

--- webapp.py
+++ webapp.py
@@ -6,11 +6,11 @@ from flask import session
 app = Flask(__name__)
 
 # In order to use "sessions",you need a "secret key".
-# This is something random you generate.  
+# This is something random you generate.
 # For more info see: https://flask.palletsprojects.com/en/1.1.x/config/#SECRET_KEY
 
-app.secret_key=os.environ["SECRET_KEY"]; #This is an environment variable.  
-                                     #The value should be set in Heroku (Settings->Config Vars).  
+app.secret_key=os.environ["SECRET_KEY"]; #This is an environment variable.
+                                     #The value should be set in Heroku (Settings->Config Vars).
                                      #To run locally, set in env.sh and include that file in gitignore so the secret key is not made public.
 
 @app.route('/')
@@ -28,14 +28,14 @@ def renderPage1():
 
 @app.route('/page2',methods=['GET','POST'])
 def renderPage2():
-    session["firstName"]=request.form['firstName']
-    session["lastName"]=request.form['lastName']
+    session["fav_car"]=request.form['fav_car']
+    session["fav_food"]=request.form['fav_food']
     return render_template('page2.html')
 
 @app.route('/page3',methods=['GET','POST'])
 def renderPage3():
-    session["favoriteColor"]=request.form['favoriteColor']
+    session["fav_state"]=request.form['fav_state']
     return render_template('page3.html')
-    
+
 if __name__=="__main__":
     app.run(debug=False)
